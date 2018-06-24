25817882791dacf4d05e243e76da6ebede8c0f99
kharazzoro <kha.1991.14.2017@gmail.com>
2018-06-24 22:02:55 +0100

    create categories route

--- server/server.js
+++ server/server.js
@@ -1,35 +1,50 @@
-'use strict';
+"use strict";
 
-const Hapi=require('hapi');
+const Hapi = require("hapi");
 
 // Create a server with a host and port
-const server=Hapi.server({
-    host:'localhost',
-    port:8000
+const server = Hapi.server({
+  host: "localhost",
+  port: 8000
 });
 
 // Add the route
 server.route({
-    method:'GET',
-    path:'/',
-    handler:function(request,h) {
+  method: "GET",
+  path: "/",
+  handler: function(request, h) {
+    return "Legodi server";
+  }
+});
 
-        return'Legodi server';
-    }
+// Add Categories route
+
+server.route({
+  method: "GET",
+  path: "/categories",
+  handler: function(request, h) {
+    return [
+        { name: "Sport" },
+        { name: "Parks" },
+        { name: "Food" },
+        { name: "Shopping" },
+        { name: "Welcome" },
+        { name: "Asylum" }
+    
+      ]
+  }
 });
 
 // Start the server
 async function start() {
+  try {
+    await server.start();
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
 
-    try {
-        await server.start();
-    }
-    catch (err) {
-        console.log(err);
-        process.exit(1);
-    }
-
-    console.log('Server running at:', server.info.uri);
-};
+  console.log("Server running at:", server.info.uri);
+}
 
 start();
