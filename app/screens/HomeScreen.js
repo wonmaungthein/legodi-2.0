db7233d75b941391f55de06cadf4dd7d333e9963
Islam Dudaev <islamwolf23@gmail.com>
2018-06-24 21:32:43 +0100

    leave the picture in homescreen

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -21,6 +21,16 @@ export default class HomeScreen extends React.Component {
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+          <View style={styles.welcomeContainer}>
+            <Image
+              source={
+                __DEV__
+                  ? require('../assets/images/robot-dev.png')
+                  : require('../assets/images/robot-prod.png')
+              }
+              style={styles.welcomeImage}
+            />
+          </View>
 
           <View>
               <Text style={styles.legodiTitle}>Legodi</Text>
