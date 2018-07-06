890efd3ea536da9d788403bad2baee60b7745c87
Islam Dudaev <islamwolf23@gmail.com>
2018-07-06 01:13:39 +0100

    style the categories in HomeScreen

--- app/components/CategoriesIcon.js
+++ app/components/CategoriesIcon.js
@@ -1,12 +1,71 @@
 import React from 'react'
 import {
-  Text
+  Text,
+  StyleSheet,
+  TouchableOpacity
 } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 
 const CategoriesList = (props) => {
+  const iconName = (name) => {
+    let str = ''
+    switch (name) {
+      case 'Welcome':
+        str = 'md-information-circle'
+        break
+      case 'Asylum':
+        str = 'md-paper'
+        break
+      case 'Volunteering':
+        str = 'md-briefcase'
+        break
+      case 'Food':
+        str = 'md-pizza'
+        break
+      case 'Parks':
+        str = 'md-paw'
+        break
+      case 'Sport':
+        str = 'md-tennisball'
+        break
+      case 'Shopping':
+        str = 'md-cart'
+        break
+      case 'Transport':
+        str = 'md-bus'
+        break
+      case 'Children':
+        str = 'md-contacts'
+        break
+      case 'Emergency':
+        str = 'md-warning'
+        break
+    }
+    return str
+  }
+
   return (
-    <Text>{props.title}</Text>
+    <TouchableOpacity style={styles.box}>
+      <Ionicons name={iconName(props.title)} size={36} color='#e5ba4f' />
+      <Text style={styles.title}>{props.title}</Text>
+    </TouchableOpacity>
   )
 }
 
 export default CategoriesList
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    marginTop: 20,
+    backgroundColor: '#245e55',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    color: '#fff',
+    fontWeight: 'bold',
+    paddingTop: 10
+  }
+})
--- app/screens/CategoriesList.js
+++ app/screens/CategoriesList.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import {
-  View
-} from 'react-native'
+import { ScrollView, StyleSheet, View } from 'react-native'
 import * as api from '../helpers/api'
 import CategoriesIcon from '../components/CategoriesIcon'
 
@@ -18,15 +16,32 @@ export default class CategoriesList extends React.Component {
     this.setState({ categories })
   }
 
+  renderCategories = () => {
+    return this.state.categories.map((category, i) => <CategoriesIcon key={i} title={category.title} />)
+  }
+
   render () {
     return (
-      <View>
-        {this.state.categories.map((category, i) => {
-          return (
-            <CategoriesIcon title={category.title} key={i} />
-          )
-        })}
-      </View>
+      <ScrollView style={styles.container}>
+        <View style={styles.layout}>
+          {this.renderCategories()}
+        </View>
+      </ScrollView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e5ba4f'
+  },
+  layout: {
+    flex: 1,
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginTop: 20
+  }
+})
