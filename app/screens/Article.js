553989e5830adc63c59b3ce32c5bb1a10ed99281
Islam Dudaev <islamwolf23@gmail.com>
2018-07-09 19:53:13 +0100

    add Article screen and remove unnecessary styles in HomeScreen

---
+++ app/screens/Article.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {
+  Text,
+  View
+} from 'react-native'
+
+export default class Article extends React.Component {
+  static navigationOptions = {
+    title: 'Glasgow welcome back',
+    headerStyle: { backgroundColor: '#0f352f' },
+    headerTitleStyle: { color: '#e6bc44' }
+  };
+
+  render () {
+    return (
+      <View>
+        <Text>Weegie Game Screen</Text>
+      </View>
+    )
+  }
+}
