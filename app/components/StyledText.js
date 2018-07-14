de6e15df1bb1dc18aef9d67ec6d2a581f8fbc90e
Islam Dudaev <islamwolf23@gmail.com>
2018-07-15 00:38:08 +0100

    finish propTypes with components folder

--- app/components/StyledText.js
+++ app/components/StyledText.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Text } from 'react-native'
+import PropTypes from 'prop-types'
 
 export class MonoText extends React.Component {
-  render () {
+  render() {
     return <Text {...this.props} style={[this.props.style, { fontFamily: 'space-mono' }]} />
   }
+}
+
+MonoText.propTypes = {
+  style: PropTypes.object
 }
--- app/components/TabBarIcon.js
+++ app/components/TabBarIcon.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Icon } from 'expo'
+import PropTypes from 'prop-types';
 
 import Colors from '../constants/Colors'
 
 export default class TabBarIcon extends React.Component {
-  render () {
+  render() {
     return (
       <Icon.Ionicons
         name={this.props.name}
@@ -15,3 +16,7 @@ export default class TabBarIcon extends React.Component {
     )
   }
 }
+
+TabBarIcon.propTypes = {
+  name: PropTypes.string
+} 
