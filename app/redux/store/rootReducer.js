aec8a63562355d3a5f65c1dfc99efac7a93db0b3
kharazzoro <kha.1991.14.2017@gmail.com>
2018-07-18 14:43:02 +0100

    add WeegieGame to combineReducers

--- app/redux/store/rootReducer.js
+++ app/redux/store/rootReducer.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux'
 import categories from '../reducers/categories'
 import language from '../reducers/language'
+import WeegieGame from '../reducers/weegieGame'
 
 const rootReducer = combineReducers({
   categories,
-  language
+  language,
+  WeegieGame
 })
 
 export default rootReducer
