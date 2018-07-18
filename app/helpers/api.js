1007e03ded2bcdffd70564940c64edb7fc0a83e8
kharazzoro <kha.1991.14.2017@gmail.com>
2018-07-18 14:41:28 +0100

    axios featch getWeegieQuestions and update functiosn to use es6 pattern

--- app/helpers/api.js
+++ app/helpers/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios'
 
-export const getCategories = async (language = 'en') => {
-  return axios.get(`/api/${language}`)
-}
+export const getCategories = async (language = 'en') => axios.get(`/api/${language}`)
 
-export const getArticles = async (categoryId, language) => {
-  return axios.get(`/api/${language}/${categoryId}`)
-}
+export const getArticles = async (categoryId, language) => axios.get(`/api/${language}/${categoryId}`)
+
+export const getWeegieQuestions = async () => axios.get('/api/weegie')
