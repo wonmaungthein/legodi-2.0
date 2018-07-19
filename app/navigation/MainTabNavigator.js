eed6b75a3bd05485c64e6dc93d9f4c9d85a501e3
kharazzoro <kha.1991.14.2017@gmail.com>
2018-07-19 18:30:40 +0100

    use generateSingleStackNavigator for single screens

--- app/navigation/MainTabNavigator.js
+++ app/navigation/MainTabNavigator.js
@@ -1,4 +1,7 @@
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation'
+import {
+  createStackNavigator,
+  createBottomTabNavigator
+} from 'react-navigation'
 import Home from '../screens/HomeScreen/HomeScreen'
 import About from '../screens/AboutScreen/AboutScreen'
 import Settings from '../screens/SettingsScreen/SettingsScreen'
@@ -7,32 +10,33 @@ import Article from '../screens/Article/Article'
 import Articles from '../screens/ArticlesListScreen/ArticlesListScreen'
 import * as helper from './helpers'
 
-const HomeStack = createStackNavigator({Home, Articles, Article}, {initialRouteName: 'Home'})
+const HomeStack = createStackNavigator({ Home, Articles, Article }, { initialRouteName: 'Home' })
 helper.generateNavigation(HomeStack, 'Home', '#0f352f', 'ios-home', 'md-home')
 
-const WeegieGameStack = createStackNavigator({Game})
+const WeegieGameStack = helper.generateSingleStackNavigator(Game)
 helper.generateNavigation(WeegieGameStack, 'Game', '#0f352f', 'ios-game-controller-b', 'md-game-controller-b')
 
-const AboutScreenStack = createStackNavigator({About})
+const AboutScreenStack = helper.generateSingleStackNavigator(About)
 helper.generateNavigation(AboutScreenStack, 'About', '#0f352f', 'ios-information-circle', 'md-information-circle')
 
-const SettingsStack = createStackNavigator({Settings})
+const SettingsStack = helper.generateSingleStackNavigator(Settings)
 helper.generateNavigation(SettingsStack, 'Settings', '#0f352f', 'ios-settings', 'md-settings')
 
-export default createBottomTabNavigator({
-  HomeStack,
-  WeegieGameStack,
-  SettingsStack,
-  AboutScreenStack
-},
-{
-  navigationOptions: ({navigation}) => helper.tabBarVisibility(navigation),
+export default createBottomTabNavigator(
+  {
+    HomeStack,
+    WeegieGameStack,
+    SettingsStack,
+    AboutScreenStack
+  },
+  {
+    navigationOptions: ({ navigation }) => helper.tabBarVisibility(navigation),
 
-  tabBarOptions: {
-    activeTintColor: '#e6bc44',
-    activeBackgroundColor: '#215e55',
-    inactiveBackgroundColor: '#0f352f',
-    inactiveTintColor: '#e6bc44'
+    tabBarOptions: {
+      activeTintColor: '#e6bc44',
+      activeBackgroundColor: '#215e55',
+      inactiveBackgroundColor: '#0f352f',
+      inactiveTintColor: '#e6bc44'
+    }
   }
-}
 )
