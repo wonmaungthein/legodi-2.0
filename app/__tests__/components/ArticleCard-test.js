96e3175627022e2601ac1947e64d748c6695f6cb
kharazzoro <kha.1991.14.2017@gmail.com>
2018-07-20 13:14:36 +0100

    fix lint

--- app/__tests__/components/ArticleCard-test.js
+++ app/__tests__/components/ArticleCard-test.js
@@ -1,22 +1,22 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import { Text, TouchableOpacity, Image, View } from "react-native";
-import ArticleCard from "../../components/ArticleCard/ArticleCard";
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { Text, TouchableOpacity, Image, View } from 'react-native'
+import ArticleCard from '../../components/ArticleCard/ArticleCard'
 
-it("renders correctly", () => {
+it('renders correctly', () => {
   const tree = renderer
     .create(
       <ArticleCard>
         <TouchableOpacity style={{}}>
-          <Image style={{width:100}} source={{ uri: 'http://placehold.it/100x80' }} />{" "}
-          <View style={{paddingLeft:5}}>
-          <Text style={{fontWeight:'bold'}}>Some Text</Text>
-          <Text style={{fontWeight:'bold'}}>Some Text</Text>
-          </View> 
+          <Image style={{width: 100}} source={{ uri: 'http://placehold.it/100x80' }} />{' '}
+          <View style={{paddingLeft: 5}}>
+            <Text style={{fontWeight: 'bold'}}>Some Text</Text>
+            <Text style={{fontWeight: 'bold'}}>Some Text</Text>
+          </View>
         </TouchableOpacity>
       </ArticleCard>
     )
-    .toJSON();
+    .toJSON()
 
-  expect(tree).toMatchSnapshot();
-});
+  expect(tree).toMatchSnapshot()
+})
