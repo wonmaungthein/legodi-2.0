6c25d206b4abd93780af4d2e03da05e5771a4c20
kharazzoro <kha.1991.14.2017@gmail.com>
2018-07-26 16:24:04 +0100

    connect setting reducer to central state

--- app/redux/store/rootReducer.js
+++ app/redux/store/rootReducer.js
@@ -3,13 +3,15 @@ import categories from '../reducers/categories'
 import language from '../reducers/language'
 import {WeegieGame, WeegieGameAnswers} from '../reducers/weegieGame'
 import {City} from '../reducers/city'
+import {Setting} from '../reducers/setting'
 
 const rootReducer = combineReducers({
   categories,
   language,
   WeegieGame,
   WeegieGameAnswers,
-  City
+  City,
+  Setting
 })
 
 export default rootReducer
