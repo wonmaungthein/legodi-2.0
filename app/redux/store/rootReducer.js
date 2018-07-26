3b1312c54fced9086fbe118cc24db27fbfc3dde0
kharazzoro <kha.1991.14.2017@gmail.com>
2018-07-26 16:30:07 +0100

    remvoe city and setting from rootReducer

--- app/redux/store/rootReducer.js
+++ app/redux/store/rootReducer.js
@@ -1,16 +1,12 @@
 import {combineReducers} from 'redux'
 import categories from '../reducers/categories'
-import language from '../reducers/language'
 import {WeegieGame, WeegieGameAnswers} from '../reducers/weegieGame'
-import {City} from '../reducers/city'
 import {Setting} from '../reducers/setting'
 
 const rootReducer = combineReducers({
   categories,
-  language,
   WeegieGame,
   WeegieGameAnswers,
-  City,
   Setting
 })
 
