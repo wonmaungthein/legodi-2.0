1c59db998a653c643ecf6e4fafbf499e3b04ce42
kharazzoro <kha.1991.14.2017@gmail.com>
2018-07-26 15:45:22 +0100

    connect select city to redux and update city through redux

--- app/redux/store/rootReducer.js
+++ app/redux/store/rootReducer.js
@@ -2,12 +2,14 @@ import {combineReducers} from 'redux'
 import categories from '../reducers/categories'
 import language from '../reducers/language'
 import {WeegieGame, WeegieGameAnswers} from '../reducers/weegieGame'
+import {City} from '../reducers/city'
 
 const rootReducer = combineReducers({
   categories,
   language,
   WeegieGame,
-  WeegieGameAnswers
+  WeegieGameAnswers,
+  City
 })
 
 export default rootReducer
