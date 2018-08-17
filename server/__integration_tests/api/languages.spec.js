29a48ef0e24f6fa9f14c88393197c611041eed97
dwinatech <diwina1993@hotmail.com>
2018-08-17 17:51:05 +0100

    create usersDB connction function and users reset API

--- server/__integration_tests/api/languages.spec.js
+++ server/__integration_tests/api/languages.spec.js
@@ -4,9 +4,9 @@ const language = require('../../app')
 describe('CRUD /languages', () => {
     it('should respond with 200', () => {
         return request(language)
-        .get('/api/languages')
-        .set('Accept', 'application/json')
-        .expect(200)
+            .get('/api/languages')
+            .set('Accept', 'application/json')
+            .expect(200)
     })
 
     it('should add new languages', () => {
@@ -17,13 +17,13 @@ describe('CRUD /languages', () => {
             "originalName": "french"
         }
         return request(language)
-        .post('/api/languages')
-        .send(data)
-        .set('Accept', 'application/json')
-        .expect(200)
-        .then(response => {
-            expect(response.body.success).toEqual(true)
-        })
+            .post('/api/languages')
+            .send(data)
+            .set('Accept', 'application/json')
+            .expect(200)
+            .then(response => {
+                expect(response.body.success).toEqual(true)
+            })
     })
 
     it('should edit new languages', () => {
@@ -34,23 +34,23 @@ describe('CRUD /languages', () => {
             originalName: 'arabic'
         }
         return request(language)
-        .put('/api/languages')
-        .send(data)
-        .set('Accept', 'application/json')
-        .expect(200)
-        .then(response => {
-            expect(response.body.success).toEqual(true)
-        })
+            .put('/api/languages')
+            .send(data)
+            .set('Accept', 'application/json')
+            .expect(200)
+            .then(response => {
+                expect(response.body.success).toEqual(true)
+            })
     })
 
     it('should delete new languages', () => {
         const languageId = 'en';
         return request(language)
-        .delete(`/api/languages/?languageId=${languageId}`)
-        .set('Accept', 'application/json')
-        .expect(200)
-        .then(response => {
-            expect(response.body.success).toEqual(true)
-        })
+            .delete(`/api/languages/?languageId=${languageId}`)
+            .set('Accept', 'application/json')
+            .expect(200)
+            .then(response => {
+                expect(response.body.success).toEqual(true)
+            })
     })
 })
