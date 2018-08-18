ac289c60979b3ef8a5f851459dad6fcb352c77f8
kharazzoro <kha.1991.14.2017@gmail.com>
2018-08-18 18:44:45 +0100

    create getQuestion function

--- server/dbClients/weegieQuestionDB.js
+++ server/dbClients/weegieQuestionDB.js
@@ -3,8 +3,15 @@ const knex = require("./connection");
 function getQuestions() {
   return knex.select().from("weegie");
 }
- 
+
+function getQuestion(id) {
+  return knex
+    .select()
+    .from("weegie")
+    .where("question_id", "=", id);
+}
 
 module.exports = {
-   getQuestions
+  getQuestions,
+  getQuestion
 };
