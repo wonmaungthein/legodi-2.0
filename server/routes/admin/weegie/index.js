fb9fdf6da5ef03f9091529c01c45034262ca2378
kharazzoro <kha.1991.14.2017@gmail.com>
2018-08-18 20:14:43 +0100

    allow user to delete question

--- server/routes/admin/weegie/index.js
+++ server/routes/admin/weegie/index.js
@@ -1,40 +1,61 @@
-const express = require('express')
-const db = require('../../../dbClients/weegieQuestionDB')
+const express = require("express");
+const db = require("../../../dbClients/weegieQuestionDB");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/',(req, res) => res.render('weegie-menu'))
+router.get("/", async (req, res) => await res.render("weegie-menu"));
 
-router.get('/questions/view',async (req, res) => {
-     try {
-        const data = await db.getQuestions()
-         res.render('weegie-table',{data});
-    } catch (error) {
-        res.render('error',{error});
-    }
-})
+router.get("/questions/view", async (req, res) => {
+  try {
+    const data = await db.getQuestions();
+    res.render("weegie-table-view", { data });
+  } catch (error) {
+    res.render("error", { error });
+  }
+});
 
-router.get('/questions/view/:questionId',async (req, res) => {
-    const { questionId } = req.params
+router.get("/questions/view/:questionId", async (req, res) => {
+  const { questionId } = req.params;
+  try {
+    const data = await db.getQuestion(questionId);
+    res.render("weegie-question-view", { data });
+  } catch (error) {
+    res.render("error", { error });
+  }
+});
+
+router.get(
+  "/questions/add",
+  async (req, res) => await res.render("weegie-add")
+);
+
+router.post("/questions/add", async (req, res) => {
+  const { body } = req;
+  try {
+    await db.addQuestion(body);
+    res.redirect("/admin/weegie/questions/view");
+  } catch (error) {
+    res.render("error", { error });
+  }
+});
+
+router.get("/questions/edit", async (req, res) => {
     try {
-       const data = await db.getQuestion(questionId)
-       res.render('weegie-question-view',{data});
-   } catch (error) {
-       res.render('error',{error});
-   }
-})
- 
-router.get('/questions/add',(req, res) => res.render('weegie-add'))
-
-router.post('/questions/add', async (req, res) =>{
-     const {body}=req;
-       try {
-          await db.addQuestion(body)
-        res.redirect('/admin/weegie/questions/view');
+      const data = await db.getQuestions();
+      res.render("weegie-table-edit", { data });
     } catch (error) {
-        res.render('error',{error});
+      res.render("error", { error });
     }
-   
-})
- 
-module.exports = router
+  });
+
+  router.get("/questions/delete/:questionId", async (req, res) => {
+    const { questionId } = req.params;
+    try {
+        await db.deleteQuestion(questionId);
+        res.redirect("/admin/weegie/questions/edit");
+    } catch (error) {
+      res.render("error", { error });
+    }
+  });
+
+module.exports = router;
