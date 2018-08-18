a67ad3f1c625b6a34611b6d83b15e9635dc4ad73
kharazzoro <kha.1991.14.2017@gmail.com>
2018-08-18 18:45:47 +0100

    update router names

--- server/routes/admin/weegie/index.js
+++ server/routes/admin/weegie/index.js
@@ -3,15 +3,26 @@ const db = require('../../../dbClients/weegieQuestionDB')
 
 const router = express.Router()
 
-router.get('/',(req, res) => res.render('admin-weegie'))
+router.get('/',(req, res) => res.render('weegie-menu'))
 router.get('/questions/view',async (req, res) => {
      try {
         const data = await db.getQuestions()
         console.log(data)
-        res.render('admin-weegie-view',{data});
+        res.render('weegie-table',{data});
     } catch (error) {
         res.render('error',{error});
     }
 })
-// /admin/weegie/questions/view
+router.get('/questions/view/:questionId',async (req, res) => {
+    const { questionId } = req.params
+    try {
+       const data = await db.getQuestion(questionId)
+       res.render('weegie-question-view',{data});
+   } catch (error) {
+       res.render('error',{error});
+   }
+})
+
+
+ 
 module.exports = router
