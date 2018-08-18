8e2a1935c63f17e48f7814631dcea0dafc94300b
kharazzoro <kha.1991.14.2017@gmail.com>
2018-08-18 12:13:41 +0100

    create function to getQuestions

--- server/dbClients/weegieQuestionDB.js
+++ server/dbClients/weegieQuestionDB.js
@@ -1,20 +1,10 @@
-// const WeegieQuestion = require('../models/WeegieQuestions')
-// const ObjectId = require('mongodb').ObjectID
+const knex = require("./connection");
 
-// addNewQuestion = (query, callback) => {
-//   WeegieQuestion.create(query, callback)
-// }
-// findSingleQuestion = (questionId, callback) => {
-//   WeegieQuestion.findById(questionId, callback)
-// }
-// findQuestions = (query, callback) => {
-//   WeegieQuestion.find(query, callback)
-// }
-// removeQuestions = (questionId, callback) => {
-//   WeegieQuestion.remove({ '_id': ObjectId(questionId) }, callback)
-// }
-// editQuestions = (questionId, query, callback) => {
-//   WeegieQuestion.update({ '_id': ObjectId(questionId) }, query, {upsert: true}, callback)
-// }
+function getQuestions() {
+  return knex.select().from("weegie");
+}
+ 
 
-// module.exports = {addNewQuestion, findQuestions, removeQuestions, editQuestions, findSingleQuestion}
+module.exports = {
+   getQuestions
+};
