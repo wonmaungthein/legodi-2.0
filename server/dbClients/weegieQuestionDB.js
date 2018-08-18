ae46d05482c8ed27310dbb5466a576bb272cfaa6
kharazzoro <kha.1991.14.2017@gmail.com>
2018-08-18 18:53:25 +0100

    create editQuestion function

--- server/dbClients/weegieQuestionDB.js
+++ server/dbClients/weegieQuestionDB.js
@@ -11,7 +11,18 @@ function getQuestion(id) {
     .where("question_id", "=", id);
 }
 
+function editQuestion(data) {
+  return knex.table('weegie')
+    .where('question_id', '=', data.question_id)
+    .update({
+      question_id: data.question_id,
+      title: data.title,
+      answer: data.anser,
+    })
+};
+
 module.exports = {
   getQuestions,
-  getQuestion
+  getQuestion,
+  editQuestion
 };
