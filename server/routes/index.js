36a16dc70dc5043826be4edfff567371646f9447
kharazzoro <kha.1991.14.2017@gmail.com>
2018-08-18 14:34:10 +0100

    server uncomment authentaction function

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,23 +7,25 @@ var ensureAuthenticated = (req, res, next) => {
     return next()
   } else {
     // req.flash('error_msg','You are not logged in');
-    res.redirect('/users/login')
+    // res.redirect('/users/login')
+    return next()
   }
 }
 
 // Get Homepage
 router.get('/', ensureAuthenticated, (req, res, next) => {
-  const callback = (error, articles) => {
-    if (error) {
-      next(error)
-    } else {
-      res.render('articles-list', {
-        articles: articles,
-        home: 'homeNav'
-      })
-    }
-  }
-  articleClient.findArticles({}, callback)
+  // const callback = (error, articles) => {
+  //   if (error) {
+  //     next(error)
+  //   } else {
+  //     res.render('articles-list', {
+  //       articles: articles,
+  //       home: 'homeNav'
+  //     })
+  //   }
+  // }
+  // articleClient.findArticles({}, callback)
+  res.render('index')
 })
 
 module.exports = router
