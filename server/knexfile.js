ebc220201f9c31d840277dbedef8cded649284d9
Pedro <hello@thisispedro.com>
2018-08-26 12:03:59 +0100

    missing ,

--- server/knexfile.js
+++ server/knexfile.js
@@ -57,7 +57,7 @@ module.exports = {
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD
-    }
+    },
     pool: {
       min: 2,
       max: 10
