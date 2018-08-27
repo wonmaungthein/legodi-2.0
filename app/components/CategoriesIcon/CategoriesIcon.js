0f57e3a667b9037aaefbea2065b3a88666423e8c
Islam Dudaev <islamwolf23@gmail.com>
2018-08-27 22:48:12 +0100

    refactor(test colors): Change color for displaying Categories in main page
    Change color for displaying Categories in main page

--- app/components/CategoriesIcon/CategoriesIcon.js
+++ app/components/CategoriesIcon/CategoriesIcon.js
@@ -1,43 +1,57 @@
-import React from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
 import {
-  Ionicons, MaterialIcons, FontAwesome, Entypo, MaterialCommunityIcons, Foundation
-} from '@expo/vector-icons'
-import styles from './IconStyles'
-import PropTypes from 'prop-types'
+  Ionicons,
+  MaterialIcons,
+  FontAwesome,
+  Entypo,
+  MaterialCommunityIcons,
+  Foundation
+} from "@expo/vector-icons";
+import styles from "./IconStyles";
+import PropTypes from "prop-types";
+import Colors from "../../constants/Colors";
+// Color passed from the Constants => yellow
+const { primaryColor } = Colors;
 
 const CategoriesIcon = props => {
   const getIconName = name => {
     switch (name) {
-      case 'Welcome':
-        return <Entypo name='hand' size={36} color='#e5ba4f' />
-      case 'Asylum':
-        return <FontAwesome name='balance-scale' size={32} color='#e5ba4f' />
-      case 'Volunteering':
-        return <FontAwesome name='child' size={36} color='#e5ba4f' />
-      case 'Food':
-        return <MaterialCommunityIcons name='food' size={43} color='#e5ba4f' />
-      case 'Parks':
-        return <Foundation name='guide-dog' size={47} color='#e5ba4f' />
-      case 'Sport':
-        return <MaterialIcons name='directions-run' size={39} color='#e5ba4f' />
-      case 'Shopping':
-        return <Ionicons name='md-cart' size={36} color='#e5ba4f' />
-      case 'Transport':
-        return <Ionicons name='ios-train' size={40} color='#e5ba4f' />
-      case 'Children':
-        return <Ionicons name='md-contacts' size={36} color='#e5ba4f' />
-      case 'Emergency':
-        return <FontAwesome name='heartbeat' size={36} color='#e5ba4f' />
+      case "Welcome":
+        return <Entypo name="hand" size={36} color={primaryColor} />;
+      case "Asylum":
+        return (
+          <FontAwesome name="balance-scale" size={32} color={primaryColor} />
+        );
+      case "Volunteering":
+        return <FontAwesome name="child" size={36} color={primaryColor} />;
+      case "Food":
+        return (
+          <MaterialCommunityIcons name="food" size={43} color={primaryColor} />
+        );
+      case "Parks":
+        return <Foundation name="guide-dog" size={47} color={primaryColor} />;
+      case "Sport":
+        return (
+          <MaterialIcons name="directions-run" size={39} color={primaryColor} />
+        );
+      case "Shopping":
+        return <Ionicons name="md-cart" size={36} color={primaryColor} />;
+      case "Transport":
+        return <Ionicons name="ios-train" size={40} color={primaryColor} />;
+      case "Children":
+        return <Ionicons name="md-contacts" size={36} color={primaryColor} />;
+      case "Emergency":
+        return <FontAwesome name="heartbeat" size={36} color={primaryColor} />;
       default:
-        return <Ionicons name='md-help' size={36} color='#e5ba4f' />
+        return <Ionicons name="md-help" size={36} color={primaryColor} />;
     }
-  }
+  };
   return (
     <TouchableOpacity
       style={styles.box}
       onPress={() =>
-        props.navigation('Articles', {
+        props.navigation("Articles", {
           id: props.id,
           categoryTitle: props.title,
           description: props.description,
@@ -50,8 +64,8 @@ const CategoriesIcon = props => {
 
       <Text style={styles.title}>{props.title}</Text>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
 CategoriesIcon.propTypes = {
   navigation: PropTypes.func,
@@ -59,6 +73,6 @@ CategoriesIcon.propTypes = {
   categoryTitle: PropTypes.string,
   description: PropTypes.string,
   title: PropTypes.string
-}
+};
 
-export default CategoriesIcon
+export default CategoriesIcon;
--- app/components/CategoriesIcon/IconStyles.js
+++ app/components/CategoriesIcon/IconStyles.js
@@ -1,33 +1,34 @@
-import {
-  StyleSheet
-} from 'react-native'
+import { StyleSheet } from "react-native";
+import Colors from "../../constants/Colors";
+// Colors passed from the Constants => yellow and green
+const { primaryColor, secondaryColor } = Colors;
 
 const styles = StyleSheet.create({
   box: {
     width: 100,
     height: 100,
     marginTop: 20,
-    backgroundColor: '#245e55',
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: secondaryColor,
+    justifyContent: "center",
+    alignItems: "center"
   },
   title: {
-    color: '#fff',
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
     paddingTop: 10
   },
   corner: {
     width: 20,
     height: 20,
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     right: 0,
     borderWidth: 10,
-    borderBottomColor: 'black',
-    borderLeftColor: 'black',
-    borderTopColor: '#e5ba4f',
-    borderRightColor: '#e5ba4f'
+    borderBottomColor: "black",
+    borderLeftColor: "black",
+    borderTopColor: primaryColor,
+    borderRightColor: primaryColor
   }
-})
+});
 
-export default styles
+export default styles;
