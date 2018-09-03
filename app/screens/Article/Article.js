c06ea650035c66b405a591ee28da56284a19d60d
Islam Dudaev <islamwolf23@gmail.com>
2018-09-03 23:37:11 +0100

    Change title for single article screen

--- app/screens/Article/Article.js
+++ app/screens/Article/Article.js
@@ -1,22 +1,46 @@
 import React from 'react'
 import { Text, View, Image, ScrollView } from 'react-native'
 import { Constants } from 'expo'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 import styles from './ArticleStyle'
 import Colors from '../../constants/Colors'
 const { primaryColor, secondaryColor } = Colors
 
-export default class Article extends React.Component {
-  static navigationOptions = {
-    title: 'Glasgow Welcome Pack',
-    headerStyle: {
-      backgroundColor: secondaryColor,
-      paddingTop: Constants.statusBarHeight
-    },
-    headerTitleStyle: { color: primaryColor },
-    headerTintColor: primaryColor
+class Article extends React.Component {
+
+  componentDidMount() {
+    const { cities, cityId } = this.props
+    const cityName = cities.filter(city => city.city_id === cityId)[0].city_name
+    this.props.navigation.setParams({ cityName })
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { cityId } = nextProps
+    if (cityId !== this.props.cityId) {
+      const cityName = this.props.cities.filter(city => city.city_id === cityId)[0].city_name
+      this.props.navigation.setParams({ cityName })
+    }
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return this.props.cityId !== nextProps.cityId
+  }
+
+  static navigationOptions = ({ navigation }) => {
+    const { params } = navigation.state
+    return {
+      title: params ? `${params.cityName} Welcome Pack` : 'Glasgow Welcome Pack',
+      headerStyle: {
+        backgroundColor: secondaryColor,
+        paddingTop: Constants.statusBarHeight
+      },
+      headerTitleStyle: { color: primaryColor },
+      headerTintColor: primaryColor
+    }
   };
 
-  render () {
+  render() {
     const language = this.props.navigation.getParam('language', 'en')
     const description = this.props.navigation.getParam(
       'description',
@@ -54,3 +78,15 @@ export default class Article extends React.Component {
     )
   }
 }
+
+const mapStateToProps = (state) => ({
+  cityId: state.Setting.city,
+  cities: state.cities.citiesList
+})
+
+Article.propTypes = {
+  cityId: PropTypes.string,
+  cities: PropTypes.array
+}
+
+export default connect(mapStateToProps, null)(Article)
