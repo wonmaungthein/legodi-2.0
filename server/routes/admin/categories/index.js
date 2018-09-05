61b8d36bd664db14c5b688203b8ea4a179cc2c2b
dwinatech <diwina1993@hotmail.com>
2018-09-05 13:02:04 +0100

    update add and edit category endpoints

--- server/routes/admin/categories/index.js
+++ server/routes/admin/categories/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const db = require('../../../dbClients/categoriesDB')
+const languageDb = require('../../../dbClients/languageDB')
+const citiesDb = require('../../../dbClients/citiesDB')
 
 const router = express.Router()
 
@@ -34,7 +36,11 @@ router.get('/view/:categoryId', async (req, res) => {
   }
 })
 
-router.get('/add', async (req, res) => res.render('category-add'))
+router.get('/add', async (req, res) => {
+  const languages = await languageDb.getLanguages()
+  const cities = await citiesDb.getCities()
+  res.render('category-add', { languages, cities })
+})
 
 router.post('/add', async (req, res) => {
   const { body } = req
@@ -68,9 +74,13 @@ router.get('/delete/:categoryId', async (req, res) => {
 router.get('/edit/:categoryId', async (req, res) => {
   const { categoryId } = req.params
   try {
+    const languages = await languageDb.getLanguages()
+    const cities = await citiesDb.getCities()
     const category = await db.getCategoryById(categoryId)
     const data = category[0]
-    res.render('category-add', { data })
+    const language = languages.filter(language => language.language_id === data.language_id)[0].original_name
+    const city = cities.filter(city => city.city_id === data.city_id)[0].city_name
+    res.render('category-add', { data, languages, language, cities, city })
   } catch (error) {
     res.render('error', { error })
   }
