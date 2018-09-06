e99a376717d7cfdef6d91806ca962e900bc2428c
Islam Dudaev <islamwolf23@gmail.com>
2018-09-06 21:23:16 +0100

    Add seeds into cities table with different colors for different cities

--- server/seeds/data/cities.js
+++ server/seeds/data/cities.js
@@ -1,14 +1,23 @@
 module.exports = [
   {
     city_id: 'GLA',
-    city_name: 'Glasgow'
+    city_name: 'Glasgow',
+    primary_color: 'red',
+    secondary_color: '#0f352e',
+    categories_color: '#205f55'
   },
   {
     city_id: 'EDI',
-    city_name: 'Edinburgh'
+    city_name: 'Edinburgh',
+    primary_color: 'green',
+    secondary_color: '#0f352e',
+    categories_color: '#205f55'
   },
   {
     city_id: 'PAI',
-    city_name: 'Paisley'
+    city_name: 'Paisley',
+    primary_color: 'pink',
+    secondary_color: '#0f352e',
+    categories_color: '#205f55'
   }
 ]
