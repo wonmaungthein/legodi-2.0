720f5e30efdd46c50ba6731951755b158cad021c
Won Maung Thein <wonmaungthein@gmail.com>
2018-09-09 16:28:28 +0100

    updated more testing

--- server/cypress/integration/Legodi-Server-side.spec.js
+++ server/cypress/integration/Legodi-Server-side.spec.js
@@ -1,3 +1,4 @@
+
 describe('Legodi Server side testing example', () => {
   const LoginForm = () => {
     cy.visit('/')
@@ -11,113 +12,214 @@ describe('Legodi Server side testing example', () => {
     })
   }
 
-  it('should log in and go to Weegie Tab', () => {
-    LoginForm()
+  // it('should log in and go to Weegie Tab', () => {
+  //   LoginForm()
+  //   cy.get('a.nav-link')
+  //     .contains('Weegie')
+  //     .click()
+  //   cy.url().should('eq', 'http://localhost:3001/admin/weegie')
+  // })
+
+  // it('should visit to Weegie tab and click Add Button', () => {
+  //   cy.url('http://localhost:3001/admin/weegie')
+  //     .get('button.btn-primary').click()
+  //   cy.url().should('eq', 'http://localhost:3001/admin/weegie/questions/add')
+  // })
+
+  // it('should visit to add Question page and click Submit button', () => {
+  //   cy.url('http://localhost:3001/admin/weegie/questions/add')
+  //   cy.get('form').within(($form) => {
+  //     // cy.get() will only search for elements within form,
+  //     // not within the entire document
+  //     cy.get('input[id="title"]').type('wonmaungthein@gmail.com')
+  //     cy.get('input[id="choiceA"]').type('testing123!')
+  //     cy.get('input[id="choiceB"]').type('testing123!')
+  //     cy.get('input[id="choiceC"]').type('testing123!')
+  //     cy.get('input[id="choiceD"]').type('testing123!')
+  //     // cy.get('input[id="answer"]').type('testing123!')
+  //     cy.get('select')
+  //       .select('a').should('have.value', 'a')
+  //       .select('b').should('have.value', 'b')
+  //       .select('c').should('have.value', 'c')
+  //       .select('d').should('have.value', 'd')
+  //     cy.root().submit()
+  //   })
+  // })
+
+  // it('should find view icon and click within Weegie Tab', () => {
+  //   cy.get('tbody>tr>td').eq(2).click()
+  // })
+
+  // it('should find delete button and click', () => {
+  //   cy.url('http://localhost:3001/admin/weegie/questions/view/2')
+  //   cy.get('div').eq(1).find('button', 'delete').click()
+  // })
+
+  // it('should find Edit button and click within Weegie Tab', () => {
+  //   cy.get('tbody>tr>td').eq(3).click()
+  // })
+
+  // it('should click update button', () => {
+  //   cy.url('http://localhost:3001/admin/weegie/questions/edit/9')
+  //   cy.get('form').within(($form) => {
+  //     cy.root().submit()
+  //   })
+  // })
+
+
+  // // Visit to Language Tab
+  // it('should log in and go to Language Tab', () => {
+  //   LoginForm()
+  //   cy.get('a.nav-link')
+  //     .contains('Language')
+  //     .click()
+  //   cy.url().should('eq', 'http://localhost:3001/admin/language')
+  // })
+
+
+  // it('should visit to Language tab and click Add Button', () => {
+  //   cy.url('http://localhost:3001/admin/language')
+  //     .get('button.btn-primary').click()
+  //   cy.url().should('eq', 'http://localhost:3001/admin/language/add')
+  // })
+
+  // it('should visit to add Language page and click Submit button', () => {
+  //   cy.url('http://localhost:3001/admin/language/add')
+  //   cy.get('form').within(($form) => {
+  //     // cy.get() will only search for elements within form,
+  //     // not within the entire document
+  //     cy.get('input[id="languageId"]').type('wonmaungthein@gmail.com')
+  //     cy.get('input[id="originalName"]').type('testing123!')
+  //     cy.get('input[id="longName"]').type('testing1!')
+  //     cy.get('input[id="shortName"]').type('testi23!')
+  //     cy.root().submit()
+  //   })
+  // })
+
+  // it('should find Edit icon and click within language Tab', () => {
+  //   cy.get('a.nav-link')
+  //     .contains('Language')
+  //     .click()
+  //   cy.url().should('eq', 'http://localhost:3001/admin/language')
+  //   cy.get('tbody>tr>td').eq(2)
+  //     .click()
+  // })
+
+  // it('should click submit button', () => {
+  //   cy.url('http://localhost:3001/admin/weegie/questions/view/2')
+  //   cy.get('form').within(($form) => {
+  //     cy.root().submit()
+  //   })
+  // })
+
+  // it('should find Delete button and click within language Tab', () => {
+  //   cy.get('tbody>tr>td').eq(3).click()
+  // })
+
+
+  // Visit Users Tab
+
+  // const UsersTab = () => {
+  //   cy.get('a.nav-link')
+  //     .contains('Users')
+  //     .click()
+  // }
+
+  // it('should log in and go to Users Tab', () => {
+  //   LoginForm()
+  //   UsersTab()
+  //   cy.url().should('eq', 'http://localhost:3001/admin/users')
+  // })
+
+  // it('should find view icon and click within User Tab', () => {
+  //   cy.get('tbody>tr>td').eq(2)
+  //   // .click()
+  // })
+
+  // it('should find Edit icon and click within User Tab', () => {
+  //   cy.get('tbody>tr>td').eq(3)
+  //   // .click()
+  // })
+
+  // it('should click update button', () => {
+  //   cy.url('http://localhost:3001/admin/users/edit/2')
+  //   cy.get('form').within(($form) => {
+  //     cy.root()
+  //     // .submit()
+  //   })
+  // })
+  // it('should log in and go to Users Tab', () => {
+  //   LoginForm()
+  //   UsersTab()
+  //   cy.get('tbody>tr>td').eq(4)
+  //     .click()
+  //   cy.url().should('eq', 'http://localhost:3001/admin/users')
+  // })
+
+
+  // Visit to Article Tab
+  const ArticlesTab = () => {
     cy.get('a.nav-link')
-      .contains('Weegie')
+      .contains('Articles')
       .click()
-    cy.url().should('eq', 'http://localhost:3001/admin/weegie')
+  }
+
+  it('should visit and click Articles Tab', () => {
+    LoginForm()
+    ArticlesTab()
   })
 
-  it('should visit to Weegie tab and click Add Button',()=>{
-    cy.url('http://localhost:3001/admin/weegie')
+  it('should visit to Articles tab and click Add Button', () => {
+    cy.url('http://localhost:3001/admin/articles')
       .get('button.btn-primary').click()
-    cy.url().should('eq', 'http://localhost:3001/admin/weegie/questions/add')
+    cy.url().should('eq', 'http://localhost:3001/admin/articles/add')
   })
 
-  it('should visit to add Question page and click Submit button',()=>{
-    cy.url('http://localhost:3001/admin/weegie/questions/add')
+
+  it('should visit to add articles page and click Submit button', () => {
+    cy.url('http://localhost:3001/admin/articles/add')
     cy.get('form').within(($form) => {
       // cy.get() will only search for elements within form,
       // not within the entire document
-      cy.get('input[id="title"]').type('wonmaungthein@gmail.com')
-      cy.get('input[id="choiceA"]').type('testing123!')
-      cy.get('input[id="choiceB"]').type('testing123!')
-      cy.get('input[id="choiceC"]').type('testing123!')
-      cy.get('input[id="choiceD"]').type('testing123!')
-      // cy.get('input[id="answer"]').type('testing123!')
-      cy.get('select')
-        .select('a').should('have.value', 'a')
-        .select('b').should('have.value', 'b')
-        .select('c').should('have.value', 'c')
-        .select('d').should('have.value', 'd')
-      cy.root().submit()
+      cy.get('input[id="articleTitle"]').type('one')
+      cy.get('input[id="articleOrder"]').type('2')
+      cy.get('select#status')
+        .select('pending').should('have.value', 'pending')
+        .select('approved').should('have.value', 'approved')
+        .select('rejected').should('have.value', 'rejected')
+        .select('hidden').should('have.value', 'hidden')
+
+      cy.get('select#categoryId')
+        .select('1').should('have.value', '1')
+        .select('2').should('have.value', '2')
+        .select('3').should('have.value', '3')
+        .select('4').should('have.value', '4')
+      cy.get('textarea[id="shortContent"]').type('write shortContent')
+      cy.get('textarea[id="fullContent"]').type('write fullContent!')
+      // cy.get('input[type=file]').click()
+        // cy.root().submit()
+
     })
   })
-
-  it('should find view icon and click within Weegie Tab', () => {
-    cy.get('tbody>tr>td').eq(2).click()
-  })
-
-  it('should find delete button and click', () => {
-    cy.url('http://localhost:3001/admin/weegie/questions/view/2')
-    cy.get('div').eq(1).find('button','delete').click()
-  })
-
-  it('should find Edit button and click within Weegie Tab',()=>{
-    cy.get('tbody>tr>td').eq(3).click()
-  })
-
-  it('should click update button',()=>{
-    cy.url('http://localhost:3001/admin/weegie/questions/edit/9')
-    cy.get('form').within(($form) => {
-      cy.root().submit()
-    })
-  })
-
-
-  // Visit to Language Tab
-  it('should log in and go to Language Tab', () => {
-    LoginForm()
-    cy.get('a.nav-link')
-      .contains('Language')
-      .click()
-    cy.url().should('eq', 'http://localhost:3001/admin/language')
-  })
-
-
-  it('should visit to Language tab and click Add Button', () => {
-    cy.url('http://localhost:3001/admin/language')
-      .get('button.btn-primary').click()
-    cy.url().should('eq', 'http://localhost:3001/admin/language/add')
-  })
-
-  it('should visit to add Language page and click Submit button', () => {
-    cy.url('http://localhost:3001/admin/language/add')
-    cy.get('form').within(($form) => {
-      // cy.get() will only search for elements within form,
-      // not within the entire document
-      cy.get('input[id="languageId"]').type('wonmaungthein@gmail.com')
-      cy.get('input[id="originalName"]').type('testing123!')
-      cy.get('input[id="longName"]').type('testing1!')
-      cy.get('input[id="shortName"]').type('testi23!')
-      cy.root().submit()
-    })
-  })
-
-  it('should find Edit icon and click within language Tab', () => {
-    cy.get('a.nav-link')
-      .contains('Language')
-      .click()
-    cy.url().should('eq', 'http://localhost:3001/admin/language')
-    cy.get('tbody>tr>td').eq(2)
-      .click()
-  })
-
-  it('should click submit button',()=>{
-    cy.url('http://localhost:3001/admin/weegie/questions/view/2')
-    cy.get('form').within(($form) => {
-      cy.root().submit()
-    })
-  })
-
-  it('should find Delete button and click within language Tab',()=>{
-    cy.get('tbody>tr>td').eq(3).click()
-  })
-
-
- // Visit Users Tab
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -175,17 +277,7 @@ describe('Legodi Server side testing example', () => {
 //       .click()
 //   }
 
-//   const UsersTab = () => {
-//     cy.get('a.nav-link')
-//       .contains('Users')
-//       .click()
-//   }
 
-//   const ArticlesTab = () => {
-//     cy.get('a.nav-link')
-//       .contains('Articles')
-//       .click()
-//   }
 
 //   const CategoriesTab = () => {
 //     cy.get('a.nav-link')
@@ -275,10 +367,15 @@ describe('Legodi Server side testing example', () => {
 //     cy.get('button.btn.btn-lg.btn-danger').click()
 //   })
 
-//   //   Visit Articles Tab
-//   it('should visit and click Articles Tab', () => {
-//     ArticlesTab()
-//   })
+//   
+
+
+
+
+
+
+
+
 
 //   // it.only('should ')
 //   it('should find Add in Articles Tab and enter', () => {
