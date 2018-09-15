0733ac23c2217d90effb57176a2c64a5746f54d8
khaledkzy <khaledkzy@gmail.com>
2018-09-15 17:53:37 +0100

    fix status returnning null (#149)

--- server/__tools/crawlRightToRemain.js
+++ server/__tools/crawlRightToRemain.js
@@ -23,7 +23,7 @@ const scrapContent = (htmlPage) => {
   const fullContent = $('.callout').text()
   const shortContent = fullContent.substring(0, 50)
   const title = $('title').text().replace('Right to Remain Toolkit:', '')
-  return { title, short_content: shortContent, full_content: fullContent, category_id: 1 }
+  return { title, short_content: shortContent, full_content: fullContent, category_id: 1, status: 'approved' }
 }
 
 function wrtieToFile (json) {
